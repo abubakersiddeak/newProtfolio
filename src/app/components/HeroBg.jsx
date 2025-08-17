bd7826b90837d8e8bdb1713b690501b425ce3269
Your Name <abubakersiddeak@gmail.com>
2025-08-17 16:04:52 +0600

    add animition in hero section 3nd time

--- src/app/components/HeroBg.jsx
+++ src/app/components/HeroBg.jsx
@@ -4,7 +4,7 @@ import Spline from "@splinetool/react-spline";
 
 export default function HeroBg() {
   return (
-    <main className="absolute w-full h-full">
+    <main className="absolute w-full h-full hidden lg:block">
       <div className="w-full h-full   origin-center">
         <Spline scene="https://prod.spline.design/4wc81tzwdBCSPLD4/scene.splinecode" />
       </div>
